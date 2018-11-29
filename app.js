ce59ddb81c1b5d63696b0fe06a46c5d183c15e18
Lisa Lehrman <lisa.lehrman@gmail.com>
2018-11-29 12:37:34 -0800

    adds package.json

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.get('/', function (req, response) {
 
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
